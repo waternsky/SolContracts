11cefd094418c23a662438a21fa3e3c819ff60db
waternksy <playgamesroyale2@gmail.com>
2025-07-19 20:17:52 +0530

    chore: forge fmt on save

--- script/SimpleStorage.s.sol
+++ script/SimpleStorage.s.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { Script } from "@forge-std/src/Script.sol";
-import { SimpleStorage } from "../src/SimpleStorage.sol";
+import {Script} from "@forge-std/src/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
 
 contract SimpleStorageScript is Script {
-    
-    function run() external returns(SimpleStorage) {
+    function run() external returns (SimpleStorage) {
         vm.startBroadcast();
 
         SimpleStorage simpleStorage = new SimpleStorage();
