17d98b8f9780889e8f22f76226ef6891f03f226d
waternksy <playgamesroyale2@gmail.com>
2025-07-24 01:57:21 +0530

    feat: creating mocks for pricefeed

--- script/FundMe.s.sol
+++ script/FundMe.s.sol
@@ -3,12 +3,16 @@ pragma solidity ^0.8.19;
 
 import {Script} from "@forge-std/src/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract FundMeScript is Script {
     function run() external returns (FundMe) {
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
         vm.startBroadcast();
 
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
 
         vm.stopBroadcast();
 
