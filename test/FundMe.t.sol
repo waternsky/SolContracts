5bea53c459148005a77657082439609c3fc08105
waternksy <playgamesroyale2@gmail.com>
2025-07-24 00:38:47 +0530

    feat: some basic fundMe test

---
+++ test/FundMe.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test, console} from "@forge-std/src/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumUSD() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.i_owner(), address(this));
+    }
+}
